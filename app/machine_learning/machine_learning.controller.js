740879c37d68e1d64391fc07dad6636a59b5e115
unknown <ws.bychkova@gmail.com>
2020-06-09 19:49:16 +0300

    add diff

--- app/machine_learning/machine_learning.controller.js
+++ app/machine_learning/machine_learning.controller.js
@@ -3,7 +3,6 @@
 
     const server = require("../../server");
     var plotly = require('plotly')("eli.bychkova", "SifvqMMyBVJPmNQYrbhI");
-    const CovidRussia = server.main.model("CovidRussia");
     var iris = require('js-datasets-iris');
     var jssvm = require('js-svm');
     var svm = new jssvm.BinarySvmClassifier();
@@ -11,165 +10,133 @@
     const csv = require("csvtojson");
 
     // const dataColumns = ['infected', 'testsCount', 'testsApplication']
-    const exampleData = [{
-        region: 'Москва',
-        observed_date: new Date('2020-04-10T21:00:00.000Z'),
-        population: 10382754,
-        infected: 2,
-        tests: 15
-        // tourists: 15
-    },
-    {
-        region: 'Москва',
-        observed_date: new Date('2020-04-11T21:00:00.000Z'),
-        population: 10382754,
-        infected: 4,
-        tests: 15
-        // tourists: 15
-    },
-    {
-        region: 'Москва',
-        observed_date: new Date('2020-04-12T21:00:00.000Z'),
-        population: 10382754,
-        infected: 100,
-        tests: 15
-        // tourists: 15
-    },
-    ]
+
     // infected = infected by current day - infected by previous day
     // infectionCoefficient = infected/tourists
     // testsApplication = infected/tests
 
-    // tests = 15/
-    // const trainingData = [
-    //     [2, 15, 0.1333, 0],
-    //     [4, 16, 0.2667, 0],
-    //     [100, 13, 6.6667, 1],
-    //     [2, 13, 0.1538, 0],
-    //     [87, 100, 0.87, 1],
-    //     [95, 14, 6.7856, 1],
-    // ]
+    // var trainingData = [
+    //     [4952, 0, 0, 0],
+    //     [4322, 6, 0.0013882461823229986, 0],
+    //     [4272, 0, 0, 0],
+    //     [3231, 0, 0, 0],
+    //     [0, 302, 0, 1],
+    //     [0, 501, 0, 1],
+    //     [ 230926, 9709, 0.04204377159782788, 0 ],
+    //     [ 205302, 8926, 0.04347741376119083, 0 ],
+    //     [ 225713, 9263, 0.04103884136048876, 0 ],
 
-    /* 
-        Если 
-    */
-
-    // const testingData = [
-    //     [2, 15, 0.1333, 0],
-    //     [85, 99, 0.8585, 1],
-    //     [100, 13, 6.6667, 1],
-    //     []
-    // ]
-
-
-    // var trainingDataSize = Math.round(iris.rowCount * 0.9);// 135
-    var trainingData = [];
+    // ];
+    // var testingData = [
+    //     [320612, 8831, 0.027544196723765797, 0],
+    //     [335305, 8726, 0.026024067639909932, 0],
+    //     [332581, 8855, 0.02662509283452753, 0],
+    //     [0, 8984, 0, 1],
+    //     [0, 8985, 0, 1]
+    // ];
+    var trainingData = [
+        [0, 100, 0, 1],
+        [0, 1000, 0, 1],
+        [0, 2000, 0, 1],
+        [0, 15, 0, 1],
+        [333333, 20, 0.02780401685960535, 0],
+        [4000, 20000, 0.02780401685960535, 1],
+        [5000, 21000, 0.02780401685960535, 0],
+        [5000, 20, 0.02780401685960535, 1],
+    ];
     var testingData = [];
 
+    var svm = new jssvm.BinarySvmClassifier({
+        alpha: 0.01,
+        iterations: 1000,
+        C: 5.0,
+        trace: false
+    });
+
+
+    const calcDiffPercent = (a, b) => {
+        let percent;
+        if (a < b) {
+            percent = (b - a) / a * 100
+        } else {
+            percent = (a - b) / a * 100
+        }
+
+        return percent;
+    }
 
     async function train(req, res, next) {
         try {
             const cities = await csv().fromFile('data/owid-covid-data.csv');
             if (cities) {
-
                 const tests = cities.filter(city => city.location === 'Russia');
-                const data = tests.map((test, index) => {
-                    const newTests = Number(test.new_tests)
-                    const newCases = Number(test.new_cases)
+                for (let i = 70; i < tests.length; i++) {
+
+                    const newTests = Number(tests[i].new_tests)
+                    const newCases = Number(tests[i].new_cases)
+                    const prevCases = Number(tests[i - 1].new_cases)
+                    const casesDiffDay = calcDiffPercent(prevCases, newCases)
 
                     let row = [];
-                    if (index < 100) {
-                        row.push(newTests)
-                        row.push(newCases)
-                        row.push(newTests === 0 ? 0 : newCases / newTests)
-                        if (newCases  && !newTests ) {
-                            row.push(1.0)
-                        } else {
-                            row.push(0.0)
-                        }
-                    } else {
-                        
-                    }
+                    row.push(newTests)
+                    row.push(newCases)
+                    row.push(newTests === 0 ? 0 : newCases / newTests)
 
-                    return row;
-                    // console.log('date :>> ', test.date);
-                    // console.log('total_cases :>> ', test.total_cases);
-                    // console.log('new_cases: :>> ', test.new_cases);
-                    // console.log('total_tests: :>> ', test.total_tests);
-                    // console.log('new_tests: :>> ', test.new_tests);
-                    // console.log('row :>> ', row);
-                })
-console.log('tests :>> ', tests.length);
-                console.log('data :>> ', data);
+                    if ((newCases && !newTests) || (casesDiffDay && casesDiffDay > 150)) {
+                        row.push(1.0)
+                    } else {
+                        row.push(0.0)
+                    }
+                    if (i === 144) {
+                        row = [333333, 20, 0.02780401685960535, 1]
+                    }
+                    if (i < 120) {
+                        trainingData.push(row)
+                    } else {
+                        testingData.push(row)
+                    }
+                }
+
             }
 
-            // console.log('example :>> ', example);
+            console.log('trainingData :>> ', trainingData);
+            console.log('testingData :>> ', testingData);
 
-            // console.log('iris :>> ', iris);
-            //rowCount = 150
-            // for (var i = 0; i < iris.rowCount; ++i) {
-            //     var row = [];
-            //     row.push(iris.data[i][0]); // sepalLength;
-            //     row.push(iris.data[i][1]); // sepalWidth;
-            //     row.push(iris.data[i][2]); // petalLength;
-            //     row.push(iris.data[i][3]); // petalWidth;
-            //     row.push(iris.data[i][4] == "Iris-virginica" ? 1.0 : 0.0); // output which is 1 if species is Iris-virginica; 0 otherwise
-            //     if (i < trainingDataSize) {
-            //         trainingData.push(row);
-            //     } else {
-            //         testingData.push(row);
-            //     }
-            // }
-            // console.log('trainingData :>> ', trainingData);
-            // console.log('testingData :>> ', testingData);
-
-            // var result = svm.fit(trainingData);
+            var result = svm.fit(trainingData);
 
             // console.log(result);
+            testingData.forEach(data => {
+                var predicted = svm.transform(data);
+                console.log("actual: " + data[3] + " predicted: " + predicted);
+            })
 
-            // for (var i = 0; i < testingData.length; ++i) {
-            //     var predicted = svm.transform(testingData[i]);
-            //     console.log("actual: " + testingData[i][3] + " predicted: " + predicted);
-            // }
-            // const covid = await CovidRussia.find();
 
-            // let cities = []
+            // const y0 = [0, 5, 7, 15, 200, 4]
 
-            // if (covid) {
-            //     cities = covid.filter(city => city.region === 'Ростовская область' ||
-            //         city.region === 'Москва')
-            // }
-
-            // console.log('cities :>> ', cities);
-            // var x = [];
-
-            // for (var i = 0; i < 500; i++) {
-            //     x[i] = Math.random();
-            // }
-
-            // var data = [
-            //     {
-            //         x: x,
-            //         type: "histogram"
+            // for (let i = 1; i < y0.length-1; i++) {
+            //     let percent;
+            //     if (y0[i] < y0[i + 1]) {
+            //         percent =(y0[i + 1] - y0[i])/y0[i] * 100
+            //     } else {
+            //         percent =(y0[i] - y0[i + 1])/y0[i] * 100
             //     }
-            // ];
-            // console.log('data :>> ', data);
-            // const trace = {
-            //     x: [1, 2, 14, 2],
-            //     type: 'histogram'
-            // }
-            // const data = {
-            //     x: cities.map(city => city.infected),
-            //     type: 'histogram'
-            // }
 
-            // console.log('test :>> ', test);
-            // var data = [trace];
-            // console.log('data :>> ', data);
+            //     console.log('percent :>> ', percent);
+            // }
+            // const y0 = testingData.map((data) => data[0])
+            // var trace1 = {
+            //     y: y0,
+            //     type: 'box'
+            // };
+
+            // var data = [trace1];
             // var graphOptions = { filename: "basic-box-plot", fileopt: "overwrite" };
             // plotly.plot(data, graphOptions, function (err, msg) {
-            //     res.status(200).send(msg);
+            //     console.log('msg :>> ', msg);
             // });
+
+
+
             res.status(200).send('msg');
         } catch (err) {
             res.status(400).send(err);
