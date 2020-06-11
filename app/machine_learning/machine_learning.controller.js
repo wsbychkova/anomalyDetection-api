59b151877ecbce4d7ecc972170614acc15c5b466
unknown <ws.bychkova@gmail.com>
2020-06-11 19:58:52 +0300

    add plotly

--- app/machine_learning/machine_learning.controller.js
+++ app/machine_learning/machine_learning.controller.js
@@ -1,29 +1,15 @@
 (function () {
     "use strict";
 
-    const server = require("../../server");
     var plotly = require('plotly')("eli.bychkova", "SifvqMMyBVJPmNQYrbhI");
-    var iris = require('js-datasets-iris');
     var jssvm = require('js-svm');
     var svm = new jssvm.BinarySvmClassifier();
-    iris.shuffle();
     const csv = require("csvtojson");
     const data = require('../constants')
 
 
-    // const dataColumns = ['infected', 'testsCount', 'testsApplication']
-
-    // infected = infected by current day - infected by previous day
-    // infectionCoefficient = infected/tourists
-    // testsApplication = infected/tests
-
-
-    var trainingData = [
-
-    ];
-    var testingData = [
-
-    ];
+    var trainingData = [];
+    var testingData = [];
 
     var svm = new jssvm.BinarySvmClassifier({
         alpha: 0.01,
@@ -39,79 +25,126 @@
 
     async function train(req, res, next) {
         try {
-            const cities = await csv().fromFile('data/owid-covid-data.csv');
-            const tests = cities.filter(city => city.location === 'Russia');
-            data.regressionData.forEach(data => {
-                trainingData.push([data, 0])
+            const data = await csv().fromFile('data/owid-covid-data.csv');
+            const filterData = data.filter(city => city.location === 'Russia');
+            const allData = []
+            filterData.forEach((data, index) => {
+                if (index > 72) {
+                    allData.push(Number(data.new_cases))
+                }
+                if (index === 131) allData.push(324)
+                if (index === 85) allData.push(7112)
+                if (index === 150) allData.push(531)
             })
-            for (let i = 72; i < tests.length; i++) {
-                //     const newTests = Number(tests[i].new_tests)
-                const newCases = Number(tests[i].new_cases)
-
-                let row = [];
-                //     row.push(newTests)
-                row.push(newCases)
-                testingData.push(newCases)
-
-                // if (i < 120) {
-                //     trainingData.push(row)
-                // } else {
-                //     testingData.push(row)
-                // }
-                //     // row.push(newTests === 0 ? 0 : newCases / newTests)
-                //     row.push(casesDiffDay ? casesDiffDay : 0)
-
-                //     if (
-                //         (newCases && !newTests)
-                //         || (newCases > newTests)
-                //         || (casesDiffDay > 8)
-                //     ) {
-                //         row.push(1.0)
-                //     } else {
-                //         row.push(0.0)
-                //     }
-
-                // if (i < 120) {
-                //     // trainingData.push(row)
-                // } else {
-                //     // testingData.push(row)
-                // }
-                // }
-
-            }
-
-            // console.log('trainingData :>> ', trainingData);
-            console.log('testingData :>> ', testingData);
-
-            var result = svm.fit(trainingData);
-
-            console.log(result);
-            // console.log('testingData :>> ', testingData.length);
-            testingData.forEach((data, index) => {
-                var predicted = svm.transform(data);
-                // console.log("actual: " + data[0] + " predicted: " + predicted);
-            })
-
-            const test =testingData
             let average = 0;
             let dispersia = 0;
             let deviation = 0;
 
-            for (let i = 0; i < test.length; i++) {
-                average += test[i]
-                dispersia += Math.pow((test[i] - 24.3), 2)
+            for (let i = 0; i < allData.length; i++) {
+                average += allData[i]
             }
-            average = average / test.length
-            dispersia = dispersia / (test.length - 1);
-            deviation = Math.sqrt(dispersia)
-
-            for (let i = 0; i < test.length; i++) {
-                const irwinCriterion = calcIrwinCriterion(test[i], test[i - 1], deviation)
-                console.log('irwinCriterion :>> ', irwinCriterion);
+            average = average / allData.length
+            for (let i = 0; i < allData.length; i++) {
+                dispersia += Math.pow((allData[i] - average), 2)
             }
+            dispersia = dispersia / (allData.length - 1);
+            deviation = Math.sqrt(dispersia);
+
+            let irwinCriterion = 0;
+            for (let i = 0; i < allData.length; i++) {
+                irwinCriterion = calcIrwinCriterion(allData[i], allData[i - 1], deviation)
+                let row = []
+                row.push(i)
+                row.push(allData[i])
+                row.push(irwinCriterion ? irwinCriterion : 0)
+                if (i < 70) {
+                    row.push(irwinCriterion > 1 ? 1 : 0)
+                    trainingData.push(row)
+                } else {
+                    testingData.push(row)
+                }
+
+            }
+            console.log('trainingData :>> ', trainingData);
+            console.log('testingData :>> ', testingData);
+            var result = svm.fit(trainingData);
+
+            console.log(result);
+
+            const resultData = [];
+
+            trainingData.forEach(data => resultData.push(data))
+
+            testingData.forEach((data, index) => {
+                var predicted = svm.transform(data);
+                // console.log('index: ' + index + " value: " + data[1] + " predicted: " + predicted);
+                data.push(predicted)
+                resultData.push(data)
+            })
+
+            const x1 = []
+            const x2 = []
+            const y1 = []
+            const y2 = []
+            const z1 = []
+            const z2 = []
+
+            testingData.forEach(data => {
+                if (data[3] === 0) {
+                    x1.push(data[2])
+                    y1.push(data[0])
+                    z1.push(data[3])
+                } else {
+                    x2.push(data[2])
+                    y2.push(data[0])
+                    z2.push(data[3])
+                }
+            })
 
 
-            res.status(200).send('msg');
+            let trace1 = {
+                x: x1,
+                y: y1,
+                z: z1,
+                mode: "markers",
+                marker: {
+                    size: 12,
+                    line: {
+                        color: "rgba(217, 217, 217, 0.14)",
+                        width: 0.5
+                    },
+                    opacity: 0.8
+                },
+                type: "scatter3d"
+            }
+            let trace2 = {
+                x: x2,
+                y: y2,
+                z: z2,
+                mode: "markers",
+                marker: {
+                    size: 12,
+                    line: {
+                        color: "green",
+                        width: 0.5
+                    },
+                    opacity: 0.8
+                },
+                type: "scatter3d"
+            }
+            var dataMarker = [trace1, trace2];
+
+            var layout = {margin: {
+                l: 0,
+                r: 0,
+                b: 0,
+                t: 0
+              }};
+            var graphOptions = {layout: layout, filename: "simple-3d-scatter", fileopt: "overwrite"};
+            plotly.plot(dataMarker, graphOptions, function (err, msg) {
+                res.status(200).send({resultData, msg});
+            });
+            
         } catch (err) {
             res.status(400).send(err);
         }
